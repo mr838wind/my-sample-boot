5489f90a9ee9275198024db093c97d8706d294e9
mr838wind <mr838wind@gmail.com>
2018-04-07 19:29:58 +0900

    mybatis: annotation: include example

--- src/main/java/com/wind/myapp/sample/service/SampleMapper.java
+++ src/main/java/com/wind/myapp/sample/service/SampleMapper.java
@@ -17,16 +17,21 @@ public interface SampleMapper {
 	@Select(" select * from tb_sample where id = #{id} ")
 	public SampleVO selectSampleById(@Param("id") String id);
 	
+	// include example
+	public static final String INCLUDE_WHERE =
+			 "	WHERE id IN									" 
+			+"		<foreach item='item' collection='ids'	"
+			+"			open='(' separator=',' close=')'>	"
+			+"		#{item}									"
+			+"	</foreach>									"
+	;
+	
 	// xml script version
-	@Select({"<script>",
-	"	SELECT *									", 
-	"	FROM tb_sample								",
-	"	WHERE id IN									", 
-	"		<foreach item='item' collection='ids'	",
-	"			open='(' separator=',' close=')'>	",
-	"		#{item}									",
-	"	</foreach>									",
-    "</script>"})
+	@Select({"<script>"
+	,"	SELECT *									" 
+	,"	FROM tb_sample								"
+	,INCLUDE_WHERE
+    ,"</script>"})
 	public List<SampleVO> selectSampleListForScript(@Param("ids") String[] ids);
 	
 	//selectProvider ...
