39182b95f739db58f1b67628538e11c296dd1633
mr838wind <mr838wind@gmail.com>
2018-04-10 18:16:58 +0900

    mybatis

--- src/main/java/com/wind/myapp/sample/service/SampleDAO.java
+++ src/main/java/com/wind/myapp/sample/service/SampleDAO.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 /**
  * SampleDAO: 
- * Mapper방식,SqlSession방식 모두 OK.
+ * >> Mapper방식,SqlSession방식 모두 OK.
  * 		interface using @Mapper,
  * 		class  using @Autowired SqlSession .
+ * >> @Select,xml에서 정의한것, 둘중 하나만 사용해야 함. 
  * @author windfall
  *
  */
