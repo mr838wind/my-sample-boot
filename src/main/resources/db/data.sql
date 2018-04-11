a597212097572fd773d4bf9d30500f935fe3000a
mr838wind <mr838wind@gmail.com>
2018-04-11 16:35:06 +0900

    plugin test: mybatis generator

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,4 +2,6 @@
 
 insert into tb_sample (id,name) values ('111','aaa');
 insert into tb_sample (id,name) values ('222','bbb');
+insert into tb_sample (id,name) values ('333','ccc');
+insert into tb_sample (id,name) values ('444','ddd');
 
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS tb_sample;
 
 
 CREATE TABLE tb_sample (
-  id 	varchar(200) DEFAULT NOT NULL COMMENT 'id',
-  name 	varchar(200)  DEFAULT NOT NULL COMMENT 'name',
+  id 	varchar(200) NOT NULL COMMENT 'id',
+  name 	varchar(200)  NOT NULL COMMENT 'name',
   PRIMARY KEY (id)
 );
