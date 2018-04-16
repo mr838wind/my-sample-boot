7618aa928b61c1d1d37550d9e4346a3e0d2f1cfa
mr838wind <mr838wind@gmail.com>
2018-04-16 18:15:57 +0900

    paging config

--- src/main/java/com/wind/myapp/config/MybatisPlusConfig.java
+++ src/main/java/com/wind/myapp/config/MybatisPlusConfig.java
@@ -1,22 +1,47 @@
 package com.wind.myapp.config;
 
+import java.util.List;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 @Configuration
 @MapperScan("com.wind.myapp.*.service")
-public class MybatisPlusConfig {
-
+public class MybatisPlusConfig implements WebMvcConfigurer {
 
 	/**
-     * 分页插件
+     * paging plugin
      */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
+    }
+    
+    /**
+	 * paging plugin: PROPAGATE_TRANSIENT_MARKER=true
+	 */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+        ObjectMapper objectMapper = builder.build();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        objectMapper.configure(MapperFeature.PROPAGATE_TRANSIENT_MARKER, true);// 忽略 transient 修饰的属性
+        converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
     }
 	
 }
