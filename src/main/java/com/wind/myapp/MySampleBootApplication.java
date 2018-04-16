b889c20dfe3d327fde3296fa7b3c2ad3a90328fa
mr838wind <mr838wind@gmail.com>
2018-04-16 16:52:18 +0900

    transaction setting

--- src/main/java/com/wind/myapp/MySampleBootApplication.java
+++ src/main/java/com/wind/myapp/MySampleBootApplication.java
@@ -2,8 +2,10 @@ package com.wind.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class MySampleBootApplication {
 
 	public static void main(String[] args) {
