594762a6b639caab7f5930e8c826d28e9c806175
mr838wind <mr838wind@gmail.com>
2018-04-16 17:32:27 +0900

    physical paging setting: must add interceptor

--- src/main/java/com/wind/myapp/config/MybatisPlusConfig.java
+++ src/main/java/com/wind/myapp/config/MybatisPlusConfig.java
@@ -1,11 +1,22 @@
 package com.wind.myapp.config;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
 
 @Configuration
 @MapperScan("com.wind.myapp.*.service")
 public class MybatisPlusConfig {
 
 
+	/**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+	
 }
