b86e4a320953afbf5e1de2235c6d6cf810ea28b1
mr838wind <mr838wind@gmail.com>
2018-04-16 13:08:55 +0900

    mybatis plus code generator

---
+++ src/test/java/com/wind/myapp/mybatisplus/generator/MybatisPlusCodeGenerator.java
@@ -0,0 +1,79 @@
+package com.wind.myapp.mybatisplus.generator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.wind.myapp.MySampleBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * mybatis plus code generator
+ * 
+ * TODO:  customise
+ * 
+ * @author windfall
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("prod")
+@Slf4j
+public class MybatisPlusCodeGenerator {
+
+	@Test
+    public void generateCode() {
+		log.info(">> generate >> {}", MySampleBootApplication.class.getPackage().getName());
+        String packageName = MySampleBootApplication.class.getPackage().getName();
+        boolean serviceNameStartWithI = false;//user -> UserService, 设置成true: user -> IUserService
+        generateByTables(serviceNameStartWithI, packageName, "tb_sample");
+    }
+
+    private void generateByTables(boolean serviceNameStartWithI, String packageName, String... tableNames) {
+        GlobalConfig config = new GlobalConfig();
+        String dbUrl = "jdbc:mysql://127.0.0.1:3306/sample?useUnicode=true&characterEncoding=UTF-8";
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL)
+                .setUrl(dbUrl)
+                .setUsername("root")
+                .setPassword("root")
+                .setDriverName("com.mysql.jdbc.Driver");
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig
+                .setCapitalMode(true)
+                .setEntityLombokModel(false)
+                .setDbColumnUnderline(true)
+                .setNaming(NamingStrategy.underline_to_camel)
+                .setInclude(tableNames);//修改替换成你需要的表名，多个表名传数组
+        config.setActiveRecord(false)
+                .setAuthor("wind")
+                .setOutputDir("d:\\tmp\\01_code_gen")
+                .setFileOverride(true);
+        if (!serviceNameStartWithI) {
+            config.setServiceName("%sService");
+        }
+        new AutoGenerator()
+        		.setGlobalConfig(config)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(
+                        new PackageConfig()
+                                .setParent(packageName)
+                                .setController("controller")
+                                .setEntity("entity")
+                ).execute();
+    }
+	
+}
