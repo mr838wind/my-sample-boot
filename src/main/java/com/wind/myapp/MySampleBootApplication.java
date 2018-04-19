b4c4728b0b760dea21dd65d5085d609af5cd32f1
mr838wind <mr838wind@gmail.com>
2018-04-19 23:42:31 +0900

    fix “Circular view path” exception

--- src/main/java/com/wind/myapp/MySampleBootApplication.java
+++ src/main/java/com/wind/myapp/MySampleBootApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafView;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 /**
  * configuration : .../framework/config/*
@@ -19,6 +23,17 @@ public class MySampleBootApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(MySampleBootApplication.class);
+	}
+	
+	// fix “Circular view path” exception
+	@Bean
+	public ThymeleafViewResolver viewResolver(ISpringTemplateEngine templateEngine) {
+	    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	    viewResolver.setViewClass(ThymeleafView.class); 
+	    viewResolver.setTemplateEngine(templateEngine);
+	    viewResolver.setCharacterEncoding("UTF-8");
+	    viewResolver.setOrder(0);//HERE!!
+	    return viewResolver;
 	}
 	
 	public static void main(String[] args) {
