29a10b417491c12dd77237bfaaf6bc8d84b2c05c
mr838wind <mr838wind@gmail.com>
2018-04-24 17:58:39 +0900

    vue essential sample

--- src/main/java/com/wind/myapp/sample/web/SampleHelloController.java
+++ src/main/java/com/wind/myapp/sample/web/SampleHelloController.java
@@ -1,17 +1,37 @@
 package com.wind.myapp.sample.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class SampleHelloController {
 
     private static final String template = "Hello, %s! --@RestSampleController";
 
+    /**
+     * visit /html/...
+     * @param name
+     * @param model
+     * @return
+     */
+    @GetMapping("/html/{pagePath}")
+    public ModelAndView vueTest(@PathVariable(name="pagePath") String pagePath, ModelAndView mv) {
+    	String viewName = "html/" + pagePath;
+    	log.info(">>>> viewName={}", viewName);
+    	mv.setViewName(viewName);
+    	
+    	return mv;
+    }
+    
+    
     /**
      * hello
      * @param name
@@ -19,9 +39,10 @@ public class SampleHelloController {
      * @return
      */
     @GetMapping("/sample/hello")
-    public String greeting(@RequestParam(value="name", defaultValue="World") String name, Model model) {
-    	model.addAttribute("name", name + " --@SampleController");
-    	return "hello";
+    public ModelAndView greeting(@RequestParam(value="name", defaultValue="World") String name, ModelAndView mv) {
+    	mv.setViewName("hello");
+    	mv.addObject("name", name + " --@SampleController");
+    	return mv;
     }
     
     /**
