57bbf7476889c848dd8b8d623e2ffea4ce3511bb
mr838wind <mr838wind@gmail.com>
2018-05-04 17:23:48 +0900

    vue advanced init: npm, webpack

---
+++ src/main/resources/static/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-app id="inspire">
+
+    <!-- drawer -->
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    
+    <!-- top -->
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+
+    <!-- content -->
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout row  wrap >
+          <v-flex xs10 offset-xs1>
+            <v-card dark color="purple">
+              <v-card-text>xs10 offset-xs1</v-card-text>
+            </v-card>
+          </v-flex>
+          <!-- 
+          <v-tooltip right>
+            <v-btn icon large :href="source" target="_blank" slot="activator">
+              <v-icon large>code</v-icon>
+            </v-btn>
+            <span>Source</span>
+          </v-tooltip>
+          -->
+
+
+        </v-layout>
+      </v-container>
+    </v-content>
+
+    <!-- footer -->
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2018 wind </span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: false
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
